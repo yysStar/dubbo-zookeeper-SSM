ab03bb157d695b995e2c5f0d72117ac58d7918de
yyaStar <qawsed1231010@126.com>
2020-05-09 16:44:16 +0800

    配置隐藏

--- qiyun-pay-consumer/src/main/java/com/qiyun/util/OSSClientConstants.java
+++ qiyun-pay-consumer/src/main/java/com/qiyun/util/OSSClientConstants.java
@@ -10,9 +10,9 @@ public class OSSClientConstants {
     //阿里云API的外网域名
     public static final String ENDPOINT = "oss-cn-hangzhou.aliyuncs.com";
     //阿里云API的密钥Access Key ID
-    public static final String ACCESS_KEY_ID = "LTAI6ZMZLpttdIH7";
+    public static final String ACCESS_KEY_ID = "**************";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESS_KEY_SECRET = "H8q5wtGpIIoqhNaOFRaXXnBM8l3AwG";
+    public static final String ACCESS_KEY_SECRET = "***********************";
     //阿里云API的bucket名称
     public static final String BACKET_NAME = "qyun88";
     //阿里云API的文件夹名称
